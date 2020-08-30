8dd789c6d7e3f4a421451a16948b5353cfed9284
JackoDev <35906674+jacko-dev@users.noreply.github.com>
2020-08-30 12:38:16 -0400

    making progress, still have the bullet moving

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Shoot : MonoBehaviour
 {
     public float Damage = 24f;
-    public float range = 100f;
+    public float range = 25f;
     public Camera fpsCam;
     public Text DamageUI;
     public GameObject Enemy;
+    public float speed = 20f;
     public static float EnemyHealth = 100;
-    public ParticleSystem particleLaunch;
+    public GameObject bulletModel;
+    public GameObject bulletSpawn;
+    [SerializeField]
+    private Transform _brrr;
+    [SerializeField]
+    private bool _bulletpos;
+    public SphereCollider bulletDetector;
+    int holder = 0;
+    float FinalMouseX;
     // Start is called before the first frame update
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            /*Destroy(bulletModel);*/
+            Constant();
             ShootWeapon();
         }
     }
@@ -24,27 +38,58 @@ public class Shoot : MonoBehaviour
     {
         if (Reload.CurrentMag == 0 && Reload.AllMags == 0)
         {
-
+            
         }
         else
         {
             if (Reload.ShootGunEnabled == true)
             {
-                RaycastHit HitInfo;
-                particleLaunch.Emit(1);
-                if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out HitInfo, range) && HitInfo.transform.tag != "Player")
-                {
-                    if (HitInfo.collider.gameObject == Enemy)
-                    {
-                        Damage = Random.Range(23f, 28f);
-                        EnemyHealth -= Damage;
-                        Debug.Log(EnemyHealth);
-                    }
-                }
-            } else if (Reload.ShootGunEnabled == false)
+                RaycastCheck();
+            }
+            else if (Reload.ShootGunEnabled == false)
             {
                 // Nothing!
             }
         }
+    }
+    void RaycastCheck()
+    {
+        Instantiate(bulletModel, bulletModel.transform.parent, false);
+        _bulletpos = false;
+        BulletBoom();
+        RaycastHit HitInfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward * Time.deltaTime, out HitInfo, range) && HitInfo.transform.tag != "Player")
+        {
+            if (HitInfo.collider.gameObject == Enemy)
+            {
+                Damage = Random.Range(23f, 28f);
+                EnemyHealth -= Damage;
+                Debug.Log(EnemyHealth);
+                Destroy(bulletModel);
+                _bulletpos = false;
+            }
+            else
+            {
+                _bulletpos = false;
+            }
+        }
+    }
+    void BulletBoom()
+    {
+        holder = 0;
+        while (holder < range && _bulletpos == false)
+        {
+            bulletModel.transform.Translate(FinalMouseX, 0f, speed * Time.deltaTime);
+            holder++;
+        }
+        if (holder == range)
+        {
+            Debug.Log("Holder = Range");
+        }
+    }
+    void Constant()
+    {
+        FinalMouseX = fpsCam.transform.position.x;
+        _bulletpos = true;
     }
 }
